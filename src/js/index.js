fc3457389354cf3bdcf1429daddcf06800705f1d
kyungho <epsoe@naver.com>
2021-11-25 17:03:03 +0900

    module js

---
+++ src/js/index.js
@@ -0,0 +1,148 @@
+// 요구사항 분석
+
+// 메뉴 추가
+// - 메뉴의 이름을 입력 받고 확인 버튼을 누르면 메뉴 추가
+// - 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
+// - 메뉴가 추가되고 나면 , input은 빈값으로 초기화
+// - 빈값은 입력값으로 추가되지 않는다.
+import { $ } from './utils/dom.js';
+import { store } from './store/store.js';
+
+function App() {
+  this.menu = {
+    espresso: [],
+    desert: [],
+    blended: [],
+    teavana: [],
+    frappuccino: [],
+  };
+  this.currentCategory = 'espresso';
+  this.init = () => {
+    if (store.getLocalStorage()) {
+      this.menu = store.getLocalStorage();
+    }
+    eventListener();
+    paintingTemplate();
+  };
+  const paintingTemplate = () => {
+    const template = this.menu[this.currentCategory]
+      .map((item, index) => {
+        return `
+    <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+<span class="w-100 pl-2 menu-name ${item.soldOut ? 'sold-out' : ''} ">${
+          item.text
+        }</span>
+<button
+type="button"
+class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+>
+품절
+</button>
+<button
+  type="button"
+  class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+>
+  수정
+</button>
+<button
+  type="button"
+  class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+>
+  삭제
+</button>
+</li>
+    
+    `;
+      })
+      .join('');
+    $('#menu-list').innerHTML = template;
+    updateCountMenu();
+  };
+  const updateCountMenu = () => {
+    const menuCount = this.menu[this.currentCategory].length;
+    $('.menu-count').innerText = `총 ${menuCount} 개`;
+  };
+  const addMenuName = () => {
+    if ($('#menu-input').value === '') {
+      return;
+    }
+    const menuName = $('#menu-input').value;
+    const newObj = {
+      text: menuName,
+    };
+    this.menu[this.currentCategory].push(newObj);
+    store.setLocalStorage(this.menu);
+
+    paintingTemplate();
+
+    $('#menu-input').value = '';
+  };
+  const editMenuName = (e) => {
+    const menuId = e.target.closest('li').dataset.menuId;
+    const $menuName = e.target.closest('li').querySelector('.menu-name');
+    const updatedMenuName = prompt('수정하시겠습니까?', $menuName.innerText);
+    this.menu[this.currentCategory][menuId].text = updatedMenuName;
+    store.setLocalStorage(this.menu);
+    paintingTemplate();
+  };
+  const deleteMenuName = (e) => {
+    const menuId = e.target.closest('li').dataset.menuId;
+    this.menu[this.currentCategory].splice(menuId, 1);
+    store.setLocalStorage(this.menu);
+    paintingTemplate();
+  };
+
+  const soldOutMenu = (e) => {
+    const menuId = e.target.closest('li').dataset.menuId;
+    this.menu[this.currentCategory][menuId].soldOut = !this.menu[
+      this.currentCategory
+    ][menuId].soldOut;
+    store.setLocalStorage(this.menu);
+    paintingTemplate();
+  };
+
+  const eventListener = () => {
+    $('#menu-form').addEventListener('submit', (e) => {
+      e.preventDefault();
+    });
+
+    $('#menu-input').addEventListener('keypress', (e) => {
+      if (e.key !== 'Enter') {
+        return;
+      }
+      addMenuName();
+    });
+    $('#menu-submit-button').addEventListener('click', addMenuName);
+    $('#menu-list').addEventListener('click', (e) => {
+      const targetEdit = e.target.classList.contains('menu-edit-button');
+      const targetDelete = e.target.classList.contains('menu-remove-button');
+      const targetSoldOut = e.target.classList.contains('menu-sold-out-button');
+      if (targetEdit) {
+        editMenuName(e);
+        return;
+      }
+      if (targetDelete) {
+        deleteMenuName(e);
+        return;
+      }
+      if (targetSoldOut) {
+        soldOutMenu(e);
+        return;
+      }
+    });
+    $('nav').addEventListener('click', (e) => {
+      const isCategoryButton = e.target.classList.contains(
+        'cafe-category-name'
+      );
+      if (isCategoryButton) {
+        const categoryName = e.target.dataset.categoryName;
+        this.currentCategory = categoryName;
+        $('#menu-name').innerText = `${e.target.innerText} 메뉴 관리`;
+        paintingTemplate();
+      }
+    });
+  };
+}
+
+const app = new App();
+app.init();
