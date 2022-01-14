c792e0362986db4d5dc9133646a7c11eb10b59f7
kyungho <epsoe@naver.com>
2022-01-14 16:00:05 +0900

    sphere animation

--- 01-Fundamentals-Part-1/starter/app.js
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -1,6 +1,29 @@
+// variable name conventions
+/* 
 let myName = 'kyungho';
 
 let firstName = 'lee';
 
 console.log(myName);
 console.log(firstName);
+*/
+
+// Data Type
+
+// number
+let age = 20;
+// string
+let username = 'lee';
+// Boolean
+let isFun = true;
+
+console.log(typeof age);
+console.log(typeof username);
+console.log(typeof isFun);
+// undefined
+
+// null
+
+// symbol
+
+// BigInt (es2020)
