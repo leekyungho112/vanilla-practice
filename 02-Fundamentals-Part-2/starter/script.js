31b938f8cd40bca6e18a927bb905eaf2b118489e
kyungho <epsoe@naver.com>
2022-01-31 11:18:10 +0900

    Pure Function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -7,3 +7,17 @@
 // if (hasDriversLicense) console.log('I can drive!');
 
 // Function
+
+const color_lawn = {
+  color: '#00FF00',
+  rating: 0,
+};
+
+const rateColor = (color, rating) => {
+  console.log(color);
+  console.log(rating);
+
+  return { ...color, rating };
+};
+console.log(rateColor(color_lawn, 5).rating);
+console.log(color_lawn.rating);
